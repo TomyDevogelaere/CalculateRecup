8016cf7b3f632f2738b9c2b49c6dc7e8faa32359
Tomy Devogelaere <devogelaere.t@ti.mil.intra>
2021-01-07 10:23:04 +0100

    Initial commit

---
+++ CalculateWorkdays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CalculateWorkdays
+{
+    class Program
+    {
+        private static TimeSpan totalRecup;
+        private static TimeSpan day = new TimeSpan(7, 36, 0);
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Saldo Recup uren:  ");
+            int hours;
+            int.TryParse(Console.ReadLine(), out hours);
+            Console.WriteLine("Saldo Recup minuten:  ");
+            int minutes;
+            int.TryParse(Console.ReadLine(), out minutes);
+            totalRecup = new TimeSpan(hours, minutes, 0);
+
+            Console.WriteLine("Hoeveel dagen recup:  ");
+            int days;
+            int.TryParse(Console.ReadLine(), out days);
+
+            SubstractDays(days);
+
+            Console.ReadLine();
+
+        }
+
+        private static void SubstractDays(int aantal)
+        {
+
+            for (int i = 0; i < aantal; i++)
+
+            {
+                totalRecup = totalRecup.Subtract(day);
+            }
+
+            var h = System.Math.Floor(totalRecup.TotalHours);
+            var m = Math.Round((totalRecup.TotalHours - h) * 60);
+            Console.WriteLine($"Overblijvende uren = {h} - aantal minuten = {m}");
+        }
+    }
+}
